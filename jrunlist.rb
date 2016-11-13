01f9ee07a6892c325b45015e4a3a58f5d6299a4f
wang-q <wang-q@outlook.com>
2016-11-14 02:27:25 +0800

    Wrong sha256

--- jrunlist.rb
+++ jrunlist.rb
@@ -3,7 +3,7 @@ class Jrunlist < Formula
   homepage "https://github.com/egateam/jrunlist"
   url "http://repo1.maven.org/maven2/com/github/egateam/jrunlist/0.1.4/jrunlist-0.1.4-jar-with-dependencies.jar"
   version "0.1.4"
-  sha256 "a1d2e5bb83544267e20fc2738f91ad48d09e83c38e91a235a7fcfc0d762f91b7"
+  sha256 "371dba693a58e96618255aa31dc72020b193888348707c8ad16a203c3e7eca3d"
 
   def install
     libexec.install "jrunlist-0.1.4-jar-with-dependencies.jar"
