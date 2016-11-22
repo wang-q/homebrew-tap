c36b730b73b23435b5c795999c835f5ee5f49b23
wang-q <wang-q@outlook.com>
2016-11-23 06:05:28 +0800

    Misc changes

--- faops.rb
+++ faops.rb
@@ -2,7 +2,7 @@ class Faops < Formula
   desc "faops: Operate fasta files"
   homepage "https://github.com/wang-q/faops"
   url "https://github.com/wang-q/faops/archive/0.2.4.tar.gz"
-  version "0.2.3"
+  version "0.2.4"
   sha256 "360c7a2a8c954b1ca050010639ece3d8440c06f411064551dccaf1af214c2dfa"
 
   depends_on "homebrew/dupes/zlib"
