427b085d5bd3d944a6b7979fe138f2cb06ce61bb
wang-q <wang-q@outlook.com>
2017-01-17 05:01:46 +0800

    Wrong sha256

--- faops.rb
+++ faops.rb
@@ -3,7 +3,7 @@ class Faops < Formula
   homepage "https://github.com/wang-q/faops"
   url "https://github.com/wang-q/faops/archive/0.3.2.tar.gz"
   version "0.3.2"
-  sha256 "e1a3c1bfd70b3197da98cf28be820d057da39f16b652ffb3d79e747bea938b0a"
+  sha256 "b444a0469715f4abf8d0c927e8e6e5a379dff13e520e4735da3bbfe6055f29ce"
 
   depends_on "homebrew/dupes/zlib"
 
