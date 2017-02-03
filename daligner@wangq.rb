d7762cd25452b1dd7f9e0ddda8c5a8890a35c38b
wang-q <wang-q@outlook.com>
2017-02-03 17:39:53 +0800

    LA4Falcon from the fork of PacificBiosciences

--- daligner@wangq.rb
+++ daligner@wangq.rb
@@ -1,17 +1,17 @@
-class DalignerAT20161119 < Formula
+class DalignerATwangq < Formula
   desc "DALIGNER: Find all significant local alignments between reads"
   homepage "https://github.com/thegenemyers/DALIGNER"
   # doi "10.1007/978-3-662-44753-6_5"
   # tag "bioinformatics"
 
-  url "https://github.com/wang-q/DALIGNER/archive/20161119.tar.gz"
-  sha256 "410000c911b230d34b19a668d7e40ed78b94243dee5ebdd5340061ea95c2c6b4"
+  url "https://github.com/wang-q/DALIGNER/archive/20170203.tar.gz"
+  sha256 "612f9ffc37f28efa5cb9bf4f056f9b9b9421172800b83de93db2008c1e595c7b"
 
   head "https://github.com/wang-q/DALIGNER.git"
 
   def install
     system "make"
-    bin.install %w[daligner HPC.daligner LAcat LAcheck LAdump LAindex LAmerge LAshow LAsort LAsplit]
+    bin.install %w[daligner HPC.daligner LAcat LAcheck LAdump LAindex LAmerge LAshow LAsort LAsplit LA4Falcon]
     doc.install "README.md"
   end
 
