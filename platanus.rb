a953c6ffebe4b32cadbb82e37ad40534cbc74a05
wang-q <wang-q@outlook.com>
2017-06-04 21:03:43 +0800

    Add -fpermissive to CXXFLAGS

--- platanus.rb
+++ platanus.rb
@@ -1,21 +1,13 @@
-class LinuxRequirement < Requirement
-  fatal true
-  satisfy OS.linux?
-  def message
-    "This software only builds on Linux."
-  end
-end
-
 class Platanus < Formula
   desc "Platanus: PLATform for Assembling NUcleotide Sequences"
   homepage "http://platanus.bio.titech.ac.jp/"
   url "http://platanus.bio.titech.ac.jp/?ddownload=150"
-  version "1.2.4"
   sha256 "7a665c0834fd064525f337639bba77a4975c4e76d029dcba2e19028c9c14fbc2"
-
-  depends_on LinuxRequirement
+  version "1.2.4"
+  revision 1
 
   def install
+    inreplace "Makefile", "Wall", "Wall -fpermissive"
     system "make"
     bin.install "platanus"
   end
