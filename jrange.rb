fdba90de82a137939eef433f203e435c1ce84041
wang-q <wang-q@outlook.com>
2017-06-20 06:11:00 +0800

    jrange 0.0.7

--- jrange.rb
+++ jrange.rb
@@ -1,13 +1,13 @@
 class Jrange < Formula
   desc "`jrange` operates ranges and links of ranges on chromosomes"
   homepage "https://github.com/egateam/jrange"
-  url "https://oss.sonatype.org/content/groups/staging/com/github/egateam/jrange/0.0.6/jrange-0.0.6-jar-with-dependencies.jar"
-  version "0.0.6"
-  sha256 "02657e6195b1a9ee472aa1d67e55b267021444252ab8dcc708c7fa3d131fa568"
+  url "https://oss.sonatype.org/content/groups/staging/com/github/egateam/jrange/0.0.7/jrange-0.0.7-jar-with-dependencies.jar"
+  version "0.0.7"
+  sha256 "966f7c63352cd5e16c299fde65952c16377bf761d1a518f3e55e88419f2cdab2"
 
   def install
-    libexec.install "jrange-0.0.6-jar-with-dependencies.jar"
-    bin.write_jar_script libexec/"jrange-0.0.6-jar-with-dependencies.jar", "jrange"
+    libexec.install "jrange-0.0.7-jar-with-dependencies.jar"
+    bin.write_jar_script libexec/"jrange-0.0.7-jar-with-dependencies.jar", "jrange"
   end
 
   test do
