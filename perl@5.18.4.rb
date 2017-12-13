79928c209cfd12e3a65d25d9542e44ad2eb3840c
wang-q <wang-q@outlook.com>
2017-12-13 14:41:51 +0800

    with-test

--- perl@5.18.4.rb
+++ perl@5.18.4.rb
@@ -4,7 +4,11 @@ class PerlAT5184 < Formula
   url "https://www.cpan.org/src/5.0/perl-5.18.4.tar.gz"
   sha256 "01a4e11a9a34616396c4a77b3cef51f76a297e1a2c2c490ae6138bf0351eb29f"
 
+  revision 1
+
   keg_only :versioned_formula
+
+  option "with-test", "Build and run the test suite"
 
   def install
     args = [
@@ -19,7 +23,7 @@ class PerlAT5184 < Formula
 
     system "./Configure", *args
     system "make"
-    system "make", "test"
+    system "make", "test" if build.with? "tests"
     system "make", "install"
   end
 
