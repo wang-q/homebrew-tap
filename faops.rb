86be7f4466eda0d3e2c9a62f8f9f07d81659a1f7
wang-q <wang-q@outlook.com>
2018-03-31 03:07:39 +0800

    faops 0.8.9

--- faops.rb
+++ faops.rb
@@ -1,9 +1,9 @@
 class Faops < Formula
   desc "faops: Operate fasta files"
   homepage "https://github.com/wang-q/faops"
-  url "https://github.com/wang-q/faops/archive/0.8.8.tar.gz"
-  version "0.8.8"
-  sha256 "0ff0e68d0fc715102941960061a7599dd9ff0a080cb473d38ebcdbb86ed4ed39"
+  url "https://github.com/wang-q/faops/archive/0.8.9.tar.gz"
+  version "0.8.9"
+  sha256 "89c20a9434478c406fe27b1c8de2a7e20870052fe6b35e0d9321a2191f2e5a66"
 
   depends_on "zlib"
 
