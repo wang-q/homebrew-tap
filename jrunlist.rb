45f96bef4be99d2b8f6177db4b8fa021e6904d94
wang-q <wang-q@outlook.com>
2019-02-10 00:14:22 +0800

    jrunlist 0.1.7

--- jrunlist.rb
+++ jrunlist.rb
@@ -1,13 +1,13 @@
 class Jrunlist < Formula
   desc "`jrunlist` operates chromosome runlist files"
   homepage "https://github.com/egateam/jrunlist"
-  url "http://repo1.maven.org/maven2/com/github/egateam/jrunlist/0.1.5/jrunlist-0.1.5-jar-with-dependencies.jar"
-  version "0.1.5"
-  sha256 "3c1d078235aa468795252f7a3f3027c90e564e6b3aaff35c269ec58b31b6e1dc"
+  url "https://oss.sonatype.org/content/groups/staging/com/github/egateam/jrunlist/0.1.7/jrunlist-0.1.7-jar-with-dependencies.jar"
+  version "0.1.7"
+  sha1 "de0642c39c2e163f088560e564d41a3aab365f32"
 
   def install
-    libexec.install "jrunlist-0.1.5-jar-with-dependencies.jar"
-    bin.write_jar_script libexec/"jrunlist-0.1.5-jar-with-dependencies.jar", "jrunlist"
+    libexec.install "jrunlist-0.1.7-jar-with-dependencies.jar"
+    bin.write_jar_script libexec/"jrunlist-0.1.7-jar-with-dependencies.jar", "jrunlist"
   end
 
   test do
