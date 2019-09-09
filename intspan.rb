3783d5171ec71dace408993d5e022cd1ad7ba910
wang-q <wang-q@outlook.com>
2019-09-10 02:23:18 +0800

    Misc changes

--- intspan.rb
+++ intspan.rb
@@ -4,7 +4,7 @@ class Intspan < Formula
 
     if OS.mac?
         url "https://github.com/wang-q/intspan/releases/download/v0.4.1/intspan-macos-x64.tar.gz"
-        sha256 "7ace1f71c8f1ab01e09ea04958f6ffece31f4b04a7e4fc2457d6c12dbea549e5"
+        sha256 "464baa143eae0888f94cb903452f5690559971535ec7762fd9de6b5423fea86a"
     elsif OS.linux?
         url "https://github.com/wang-q/intspan/releases/download/v0.4.1/intspan-linux-x64.tar.gz"
         sha256 "eb5cb55f32728693c6fe5148cafab91366396eee3495ea5e324a352e96ee0941"
