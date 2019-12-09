77fc39933adef8b0cd066056c3111d0def31ec45
wang-q <wang-q@outlook.com>
2019-12-09 23:36:07 +0800

    intspan 0.4.9

--- intspan.rb
+++ intspan.rb
@@ -3,20 +3,24 @@ class Intspan < Formula
     homepage "https://github.com/wang-q/intspan"
 
     if OS.mac?
-        url "https://github.com/wang-q/intspan/releases/download/v0.4.1/intspan-macos-x64.tar.gz"
-        sha256 "464baa143eae0888f94cb903452f5690559971535ec7762fd9de6b5423fea86a"
+        url "https://github.com/wang-q/intspan/releases/download/v0.4.9/intspan-x86_64-apple-darwin.tar.gz"
+        sha256 "98da7af1d75be987bd856cda71f428bbd8e094acdd587a2a032d1895687f3669"
     elsif OS.linux?
-        url "https://github.com/wang-q/intspan/releases/download/v0.4.1/intspan-linux-x64.tar.gz"
-        sha256 "eb5cb55f32728693c6fe5148cafab91366396eee3495ea5e324a352e96ee0941"
+        url "https://github.com/wang-q/intspan/releases/download/v0.4.9/intspan-x86_64-unknown-linux-musl.tar.gz"
+        sha256 "b0bd4bba8bb0a3270665b64da0d07a7c98f25c323f451c5a808cd5271a4b27c0"
     end
 
-    version "0.4.1"
+    version "0.4.9"
 
     bottle :unneeded
 
     def install
         # already in target/
-        bin.install Dir["release/*"]
+        if OS.mac?
+            bin.install Dir["release/*"]
+        elsif OS.linux?
+            bin.install Dir["x86_64-unknown-linux-musl/release/*"]
+        end
     end
 
     test do
