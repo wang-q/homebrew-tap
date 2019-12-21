58098c3026a4fc0571169e780f50a44b5e1dc73b
wang-q <wang-q@outlook.com>
2019-12-22 01:35:31 +0800

    jrunlist

--- jrunlist.rb
+++ jrunlist.rb
@@ -1,7 +1,7 @@
 class Jrunlist < Formula
   desc "`jrunlist` operates chromosome runlist files"
   homepage "https://github.com/egateam/jrunlist"
-  url "https://oss.sonatype.org/content/groups/staging/com/github/egateam/jrunlist/0.1.7/jrunlist-0.1.7-jar-with-dependencies.jar"
+  url "https://repo1.maven.org/maven2/com/github/egateam/jrunlist/0.1.7/jrunlist-0.1.7-jar-with-dependencies.jar"
   version "0.1.7"
   sha256 "f4509e40bcdb1b024ad49c2cfe8ce5ce39bfba9e00f1726c12cd49dbf983e7bd"
 
