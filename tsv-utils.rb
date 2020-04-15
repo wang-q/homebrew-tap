76d5444167ed2605ede0319f37a6841e989a0327
wang-q <wang-q@outlook.com>
2020-04-15 23:31:17 +0800

    tsv-utils 1.6.0

--- tsv-utils.rb
+++ tsv-utils.rb
@@ -3,15 +3,14 @@ class TsvUtils < Formula
     homepage "https://github.com/eBay/tsv-utils"
 
     if OS.mac?
-        url "https://github.com/eBay/tsv-utils/releases/download/v1.5.0/tsv-utils-v1.5.0_osx-x86_64_ldc2.tar.gz"
-        sha256 "00274bff7830db698fcf5d48d8fa274cee082845bdcb5956c9a485fa3c707c3c"
+        url "https://github.com/eBay/tsv-utils/releases/download/v1.6.0/tsv-utils-v1.6.0_osx-x86_64_ldc2.tar.gz"
+        sha256 "d25f7c17612940be577eecb6431e70024f92ea5fba60444523d4d61f14f041eb"
     elsif OS.linux?
-        url "https://github.com/eBay/tsv-utils/releases/download/v1.5.0/tsv-utils-v1.5.0_linux-x86_64_ldc2.tar.gz"
-        sha256 "9c6ea552248087ef70e7705110ba4f9fe48d134b7eea94ba040f5cce16d25f81"
+        url "https://github.com/eBay/tsv-utils/releases/download/v1.6.0/tsv-utils-v1.6.0_linux-x86_64_ldc2.tar.gz"
+        sha256 "a5810e07afd48c7b4f26dfe5c7479dff3b08d3dfcb52f7c72ec227441f724e40"
     end
 
-    version "1.5.0"
-    revision 1
+    version "1.6.0"
 
     bottle :unneeded
   
