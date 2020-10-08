18bec578ec3332c1c65f049afefd6635a1e23a8c
Qiang Wang <wang-q@outlook.com>
2020-10-08 13:55:46 +0800

    Update dazz_db@20201008.rb

--- dazz_db@20201008.rb
+++ dazz_db@20201008.rb
@@ -1,11 +1,11 @@
-class DazzDbAT20190923 < Formula
+class DazzDbAT20201008 < Formula
   desc "DAZZ_DB: The Dazzler Data Base"
   homepage "https://github.com/thegenemyers/DAZZ_DB"
   # doi "10.1007/978-3-662-44753-6_5"
   # tag "bioinformatics"
 
-  url "https://github.com/wang-q/DAZZ_DB/archive/20190923.tar.gz"
-  sha256 "051ec233ed7bea6b8056555fcad3858d8eff90b55ca0e3a25b4c4dd753fdd333"
+  url "https://github.com/wang-q/DAZZ_DB/archive/20201008.tar.gz"
+  sha256 "030dc18429ccd5781a0a091903004790c8c0b5a6a066b43d02104970a1ed2eeb"
 
   head "https://github.com/wang-q/DAZZ_DB.git"
 
