1d985e8758c796defd9886d312d0113573759db7
Qiang Wang <wang-q@outlook.com>
2022-01-19 08:41:43 +0800

    faops 0.8.20

--- faops.rb
+++ faops.rb
@@ -1,9 +1,9 @@
 class Faops < Formula
   desc "faops: Operate fasta files"
   homepage "https://github.com/wang-q/faops"
-  url "https://github.com/wang-q/faops/archive/0.8.18.tar.gz"
-  version "0.8.18"
-  sha256 "c250f9745e901fc3afadd2c85bc910e2b7e68dc1febc3ed73a4afbe9d5f46ca3"
+  url "https://github.com/wang-q/faops/archive/0.8.20.tar.gz"
+  version "0.8.20"
+  sha256 "a7fb3246b50c3ce2d0c2abb015bf5374e4ae950ea198f7dbc25266ec0889d822"
 
   head do
     url "https://github.com/wang-q/faops.git"
