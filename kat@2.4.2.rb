576623c1a6457ef3ab7b7015e08ab0a0354e3272
wang-q <wang-q@outlook.com>
2022-04-03 19:30:45 +0800

    Update kat@2.4.2.rb

--- kat@2.4.2.rb
+++ kat@2.4.2.rb
@@ -1,4 +1,6 @@
 class KatAT242 < Formula
+  include Language::Python::Virtualenv
+
   desc "KAT - The K-mer Analysis Toolkit"
   homepage "https://github.com/TGAC/KAT"
   url "https://github.com/TGAC/KAT/archive/Release-2.4.2.tar.gz"
@@ -27,6 +29,11 @@ class KatAT242 < Formula
       s.gsub! "$(top_srcdir)/deps/boost/build/include", boost.opt_include
     end
 
+    venv = virtualenv_create(libexec/"vendor", "python3")
+
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{xy}/site-packages"
+
     system "./autogen.sh"
     system "./configure",
       "--disable-silent-rules",
@@ -34,6 +41,8 @@ class KatAT242 < Formula
       *std_configure_args
     system "make"
     system "make", "install"
+
+    bin.env_script_all_files libexec, PYTHONPATH: ENV["PYTHONPATH"]
   end
 
   def caveats; <<~EOS
