ab6dc744cb988e58aa4a40ff0c3e4ade0bb025a4
Qiang Wang <wang-q@outlook.com>
2022-04-10 21:24:59 +0800

    Update r@3.6.1.rb

--- r@3.6.1.rb
+++ r@3.6.1.rb
@@ -4,61 +4,54 @@ class RAT361 < Formula
   url "https://cran.r-project.org/src/base/R-3/R-3.6.1.tar.gz"
   sha256 "5baa9ebd3e71acecdcc3da31d9042fb174d55a42829f8315f2457080978b1389"
 
+  depends_on :linux
   depends_on "pkg-config" => :build
   depends_on "gcc" # for gfortran
-  depends_on "gettext"
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "openblas"
   depends_on "pcre"
   depends_on "readline"
   depends_on "xz"
-
-  unless OS.mac?
-    depends_on "cairo"
-    depends_on "curl"
-  end
+  depends_on "zlib"
+  depends_on "bzip2"
+  depends_on "cairo"
+  depends_on "curl"
 
   # needed to preserve executable permissions on files without shebangs
   skip_clean "lib/R/bin"
 
   resource "gss" do
-    url "https://cloud.r-project.org/src/contrib/gss_2.1-10.tar.gz", :using => :nounzip
-    mirror "https://mirror.las.iastate.edu/CRAN/src/contrib/gss_2.1-10.tar.gz"
-    sha256 "26c47ecae6a9b7854a1b531c09f869cf8b813462bd8093e3618e1091ace61ee2"
+    url "https://cran.r-project.org/src/contrib/gss_2.2-3.tar.gz", :using => :nounzip
+    mirror "https://mirrors4.tuna.tsinghua.edu.cn/CRAN/src/contrib/gss_2.2-3.tar.gz"
+    sha256 "24306401cf4e5869f8a690eca7e17c044ece83edd66969bd2daf5976272d244b"
   end
 
   def install
-    # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
-    if MacOS.version == "10.11" && MacOS::Xcode.installed? &&
-       MacOS::Xcode.version >= "8.0"
-      ENV["ac_cv_have_decl_clock_gettime"] = "no"
-    end
-
     args = [
       "--prefix=#{prefix}",
       "--enable-memory-profiling",
-      "--with-lapack",
       "--enable-R-shlib",
+      "--with-lapack",
       "--disable-java",
       "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
       "--without-x",
+      "--without-tcltk",
+      "--without-ICU",
     ]
 
     # don't remember Homebrew's sed shim
     args << "SED=/usr/bin/sed" if File.exist?("/usr/bin/sed")
 
-    unless OS.mac?
-      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
-      args << "--with-cairo"
+    args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+    args << "--with-cairo"
 
-      # If LDFLAGS contains any -L options, configure sets LD_LIBRARY_PATH to
-      # search those directories. Remove -LHOMEBREW_PREFIX/lib from LDFLAGS.
-      ENV.remove "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
-    end
+    # If LDFLAGS contains any -L options, configure sets LD_LIBRARY_PATH to
+    # search those directories. Remove -LHOMEBREW_PREFIX/lib from LDFLAGS.
+    ENV.remove "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
 
     # Help CRAN packages find gettext and readline
-    ["gettext", "readline"].each do |f|
+    ["readline"].each do |f|
       ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
       ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
     end
