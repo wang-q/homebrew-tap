481604f75f66c93e7c5722684331897ccf024462
Qiang Wang <wang-q@outlook.com>
2022-07-10 17:06:22 +0800

    Create meme@4.rb

---
+++ meme@4.rb
@@ -0,0 +1,30 @@
+class Meme < Formula
+  # cite Bailey_2009: "https://doi.org/10.1093/nar/gkp335"
+  desc "Tools for motif discovery"
+  homepage "http://meme-suite.org"
+  url "http://meme-suite.org/meme-software/4.11.2/meme_4.11.2_2.tar.gz"
+  sha256 "377238c2a9dda64e01ffae8ecdbc1492c100df9b0f84132d50c1cf2f68921b22"
+
+  def install
+    ENV.deparallelize
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{libexec}"
+    system "make", "install"
+    prefix.install "tests"
+    perl_files = `grep -l -w "#!/usr/bin/perl" #{bin}/*`.split("\n")
+    perl_files.each do |file|
+      inreplace file, %r{^#!/usr/bin/perl.*}, "#!/usr/bin/env perl"
+    end
+
+    if OS.mac?
+      bin.install_symlink libexec/"bin/meme"
+    else
+      ENV["PERL5LIB"] = libexec/"lib/perl5"
+      system "cpanm", "--self-contained", "-l", libexec, "XML::Parser::Expat"
+      (bin/"meme").write_env_script(libexec/"bin/meme", :PERL5LIB => ENV["PERL5LIB"])
+    end
+  end
+
+  test do
+    system bin/"meme", prefix/"tests/common/At.s"
+  end
+end
