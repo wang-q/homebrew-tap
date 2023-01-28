468410fbe019bc3925ad03713c3e41ff4e7a6c80
Qiang Wang <wang-q@outlook.com>
2023-01-29 00:51:57 +0800

    nwr 0.5.10

--- nwr.rb
+++ nwr.rb
@@ -3,14 +3,14 @@ class Nwr < Formula
     homepage "https://github.com/wang-q/nwr"
 
     if OS.mac?
-        url "https://github.com/wang-q/nwr/releases/download/v0.5.7/nwr-x86_64-apple-darwin.tar.gz"
-        sha256 "3e75073a8733ffa349354713661fca749f26d7597ee7127355937039b91d45f8"
+        url "https://github.com/wang-q/nwr/releases/download/v0.5.10/nwr-x86_64-apple-darwin.tar.gz"
+        sha256 "a852891f056ca1b1b71fb7a35a5a134d74cf6a2a365302aa7bca6e5ac49d4b77"
     elsif OS.linux?
-        url "https://github.com/wang-q/nwr/releases/download/v0.5.7/nwr-x86_64-unknown-linux-gnu.tar.gz"
-        sha256 "354224aa864ad5744cbc8d9cdfa8a58b5b4c4ef16cee178b98dad4315242fe25"
+        url "https://github.com/wang-q/nwr/releases/download/v0.5.10/nwr-x86_64-unknown-linux-gnu.tar.gz"
+        sha256 "5364469579ba93f8f656376546c85093e8f49f57223b2e7ecc462f410ed7aca2"
     end
 
-    version "0.5.7"
+    version "0.5.10"
 
     def install
         # already in target/
