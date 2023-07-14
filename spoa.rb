bfd60869a9d3e92a2d687483d7da12263b33db92
Qiang Wang <wang-q@outlook.com>
2023-07-15 05:02:15 +0800

    spoa 4.0.7

--- spoa.rb
+++ spoa.rb
@@ -1,9 +1,9 @@
 class Spoa < Formula
   desc "PHylogenetic Analysis with Space/Time models"
   homepage "https://github.com/rvaser/spoa"
-  url "https://github.com/rvaser/spoa/releases/download/4.0.3/spoa-v4.0.3.tar.gz"
-  version "4.0.3"
-  sha256 "c88848b35f1350748ba45422f08e6e7a788fcc0d241b9fa3704ebb6b73e9d6e8"
+  url "https://github.com/rvaser/spoa/releases/download/4.0.7/spoa-v4.0.7.tar.gz"
+  version "4.0.7"
+  sha256 "f7a8cd039e4aabea1481e123387fedaa8f87e55418a3649408a615f5c6b1b9a4"
 
   depends_on "zlib"
   depends_on "cmake" => :build
