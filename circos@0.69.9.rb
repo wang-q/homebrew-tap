fa1fd0471205b01dce39517dd34ab6635de5e4be
Qiang Wang <wang-q@outlook.com>
2024-06-26 22:24:47 +0800

    Update circos@0.69.9.rb

--- circos@0.69.9.rb
+++ circos@0.69.9.rb
@@ -2,7 +2,7 @@ class CircosAT0699 < Formula
   # cite Krzywinski_2009: "https://doi.org/10.1101/gr.092759.109"
   desc "Visualize data in a circular layout"
   homepage "http://circos.ca"
-  url "http://circos.ca/distribution/circos-0.69-9.tgz"
+  url "https://circos.ca/distribution/circos-0.69-9.tgz"
   sha256 "34d8d7ebebf3f553d62820f8f4a0a57814b610341f836b4740c46c3057f789d2"
   version "0.69.9"
 
